4fe1d7555e1ad2d761edef8a2f9ebbd9f2e530c8
NghiaBV <nghiabv@fsoft.com.vn>
2014-12-04 08:33:48 +0700

    Fix WrapTextView

--- TextView/WrapTextView.java
+++ TextView/WrapTextView.java
@@ -1,11 +1,25 @@
-package jp.shopch.shopchannel.widget;
+package android.issues.textview;
+/*
+	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 import android.content.Context;
 import android.text.*;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
 /**
+ * Wrapping text in TextView
  * author: Tozawa
  */
 public class WrapTextView extends TextView {
